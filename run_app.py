3a385218b08defd909ea801c78fdfd2bd452d884
Devin Quinn <76824496+dquinn6@users.noreply.github.com>
2024-03-29 22:02:04 -0400

    Update run_app.py
    Refactored for enhanced UI

--- run_app.py
+++ run_app.py
@@ -8,24 +8,28 @@ from src.communicators import GPTCommunicator
 from src.data_processors import WikiTextProcessor
 from src.vectorstore_handlers import LangchainVectorstore
 from langchain_community.embeddings import HuggingFaceEmbeddings
-
-with open('config/manipulate_patterns.json') as f:
-    PATTERNS = list(json.load(f).items())
- 
-API_KEY = config.user_config["ACCESS_TOKEN"]
-MODEL_NAME = config.user_config["MODEL_NAME"]
-SAVE_PATH = config.user_config["SAVE_PATH"]
-SEARCH_TYPE = config.user_config["SEARCH_TYPE"]
-N_RETRIEVED_DOCS = config.user_config["N_RETRIEVED_DOCS"]
-TOKEN_LIMIT = config.user_config["TOKEN_LIMIT"]
-VERBOSE = config.user_config["VERBOSE"]
+import pandas as pd
+import sys
 
 LOG_PATH = "logs/"
+PATTERNS_FILENAME = 'config/manipulate_patterns.json'
+
+# Direct src code logging to backend.log through logging.config
+ 
+logging.basicConfig(
+    filename=LOG_PATH+"backend.log",
+    filemode='a',
+    format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
+    datefmt='%H:%M:%S',
+    level=logging.INFO
+)
+
+# Direct UI logging to streamlit.log through logger object
 
 def create_logger(
     name: str = "logger_",
-    level: str = "DEBUG",
-    filename: str = "streamlit.log",
+    level: str = "INFO",
+    filename: str = LOG_PATH+"streamlit.log",
     max_log_size: int = 1024 * 1024 * 1024, #100 MB
     backup_count: int = 1,
 ):
@@ -52,7 +56,7 @@ def create_logger(
         sum([isinstance(handler, RotatingFileHandler) for handler in logger.handlers]) == 0
     ):
         handler = RotatingFileHandler(
-            LOG_PATH + filename, maxBytes=max_log_size, backupCount=backup_count,
+            filename, maxBytes=max_log_size, backupCount=backup_count,
         )
         handler.setFormatter(
             logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
@@ -61,39 +65,83 @@ def create_logger(
     
     return logger
 
+def update_patterns_json(key = "", val = "", clear_json=False):
 
-@st.cache_resource #run only once
-def init_RAG():
+    with open(PATTERNS_FILENAME) as r:
+        patterns_json = json.load(r)
 
-    communicator = GPTCommunicator(api_key=API_KEY, model_name="gpt-3.5-turbo")
+    if clear_json:
+        patterns_json = {}
+    else:
+        patterns_json.update({key: val})
 
-    processor = WikiTextProcessor(
+    with open(PATTERNS_FILENAME, "w") as w:
+        json.dump(patterns_json, w)
+
+@st.cache_data
+def init_session():
+    # clear logs
+    with open(LOG_PATH+"streamlit.log", "w") as _:
+        pass
+    with open(LOG_PATH+"backend.log", "w") as _:
+        pass
+    #clear patterns file
+    update_patterns_json(clear_json=True)
+
+@st.cache_resource
+def init_communicator_and_processor():
+
+    api_key = config.user_config["ACCESS_TOKEN"]
+    model_name = config.user_config["MODEL_NAME"]
+    verbose = config.user_config["VERBOSE"]
+
+    communicator = GPTCommunicator(api_key=api_key, model_name=model_name)
+
+    data_processor = WikiTextProcessor(
         dataset_version = "wikitext-2-raw-v1", 
         split = "train", 
         communicator = communicator,
-        verbose = VERBOSE
+        verbose = verbose
     )
-    _ = processor.process_text(
-        token_limit = TOKEN_LIMIT, 
-        save_path = SAVE_PATH,
+    return communicator, data_processor
+
+def process_data(data_processor):
+
+    save_path = config.user_config["SAVE_PATH"]
+    token_limit = config.user_config["TOKEN_LIMIT"]
+
+    with open(PATTERNS_FILENAME) as f:
+        patterns = list(json.load(f).items())
+    
+    _ = data_processor.process_text(
+        token_limit = token_limit, 
+        save_path = save_path,
         save_filename = "processed_data.csv",
-        manipulate_pattern = PATTERNS
+        manipulate_pattern = patterns
     )
+
+def attach_vectorstore(communicator, load_vectorstore=True, _callback=None):
+
+    save_path = config.user_config["SAVE_PATH"]
+    search_type = config.user_config["SEARCH_TYPE"]
+    n_retrieved_docs = config.user_config["N_RETRIEVED_DOCS"]
+    verbose = config.user_config["VERBOSE"]
 
     vs = LangchainVectorstore(
         embedding_type = HuggingFaceEmbeddings(),
-        processed_csv_path = SAVE_PATH+"processed_data.csv",
-        verbose = VERBOSE
+        processed_csv_path = save_path+"processed_data.csv",
+        verbose = verbose
     )
-    if not os.path.exists(SAVE_PATH):
-        vs.create_local_vectorstore(save_path=SAVE_PATH)
+
+    if load_vectorstore:
+        vs.load_local_vectorstore(load_path=save_path)
     else:
-        vs.load_local_vectorstore(load_path=SAVE_PATH)
+        vs.create_local_vectorstore(save_path=save_path, force_create=True, callback=_callback)
 
     vs.create_retriever(
-        search_type=SEARCH_TYPE,
+        search_type=search_type,
         search_kwargs={
-            "k": N_RETRIEVED_DOCS
+            "k": n_retrieved_docs
         }
     )
 
@@ -101,55 +149,121 @@ def init_RAG():
 
     return communicator
 
-
 def run_streamlit_app():
 
     st.set_page_config(page_title="Page")
-
-    communicator = init_RAG()
 
     if "logger" not in st.session_state:
         st.session_state["logger"] = create_logger()
     logger = st.session_state["logger"]
 
+    init_session()
+
+    communicator, data_processor = init_communicator_and_processor()
+
     st.title("Title")
+    
+    st.header("Initialization")
 
-    st.header("header")
+    # Document manipulation through Streamlit UI
 
-    st.subheader("subheader")
+    st.subheader("Document Manipulation")
 
-    selected = st.selectbox(
-        "Select an option:",
-        ["Select"] + ["Option 1", "Option 2"]
-    )
+    st.sidebar.header("Manipulate Documents")
 
-    logger.info(f"selected: {selected}")
+    search_pattern_col, replace_pattern_col = st.sidebar.columns(2)
+    search_pattern = search_pattern_col.text_input("Search Pattern")
+    replace_pattern = replace_pattern_col.text_input("Replace Pattern")
 
-    user_query = st.text_input("Query: ")
+    if st.sidebar.button("Add Pattern"):
+        update_patterns_json(key=search_pattern, val=replace_pattern)
 
-    if st.button("Get Answer"):
-        if user_query:
+    if st.sidebar.button("Clear Patterns"):
+        update_patterns_json(clear_json=True)
 
-            # do rag
-            response, retrieved_context = communicator.post_rag_prompt(user_query)
+    with open(PATTERNS_FILENAME) as r:
+        patterns_json = json.load(r)
 
-            st.subheader("Response:")
-            st.write(response)
+    data = {"search": patterns_json.keys(), "replace": patterns_json.values()}
 
-            logger.info(f"User query: {user_query}")
+    st.write(pd.DataFrame(data))
 
-            # Get user feedback
+    # Adjust params
 
-            feedback = st.radio(
-                "Select an option:",
-                [
-                    "None",
-                    "Yes",
-                    "No",
-                ],
-            )
-            if feedback != "None":
-                logging.info(feedback)
+    st.subheader("Changed Config Params")
+    
+    st.sidebar.header("Adjust Config")
+    token_limit = st.sidebar.text_input("Token Limit")
+    
+    if token_limit:
+        token_limit = int(token_limit)
+        config.user_config["TOKEN_LIMIT"] = token_limit
+        st.write(f"Token limit: {token_limit}")
 
+    # Vectorstore init with manipulated docs
+
+    st.header("Chatbot QA")
+
+    load_vs, create_vs = st.columns(2)
+    # load_vs = st.button("Load Vectorstore")
+    # create_vs = st.button("Create New Vectorstore")
+
+    if load_vs.button("Load vectorstore"):
+        process_data(data_processor)
+        communicator = attach_vectorstore(communicator, load_vectorstore=True)
+        st.session_state["communicator"] = communicator
+
+    elif create_vs.button("Create new vectorstore"):
+        process_data(data_processor)
+        result_holder = st.empty()
+
+        def progress(p, i):
+            with result_holder.container():
+                st.progress(p, f'Progress: Documents Processed={i}')
+
+        communicator = attach_vectorstore(communicator, load_vectorstore=False, _callback=progress)
+        st.session_state["communicator"] = communicator
+
+    else:
+        if "communicator" not in st.session_state:
+            st.session_state["communicator"] = None
+    
+    if st.session_state["communicator"] != None:
+
+        #n_patterns = st.sidebar.slider("Manipulate Patterns:", min_value=1, max_value=10)
+
+        user_query = st.text_input("Query: ")
+
+        if st.button("Get Answer"):
+            if user_query:
+
+                # do rag
+                response, retrieved_context = st.session_state["communicator"].post_rag_prompt(user_query)
+
+                st.subheader("Response:")
+                st.write(response)
+
+                logger.info(f"User query: {user_query}")
+
+                # Get user feedback
+
+                feedback = st.radio(
+                    "Select an option:",
+                    [
+                        "None",
+                        "Yes",
+                        "No",
+                    ],
+                )
+                if feedback != "None":
+                    logging.info(feedback)
+
+    st.sidebar.header("Session logs")
+    with open(LOG_PATH+"streamlit.log") as log:
+        st.sidebar.write(log.readlines())     
+
+    st.sidebar.header("Developer logs")
+    with open(LOG_PATH+"backend.log") as log:
+        st.sidebar.write(log.readlines())
 
 run_streamlit_app()
