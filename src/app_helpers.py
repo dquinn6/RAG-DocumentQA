1777348dfdb7bb7abdd665c7442bdd5a518bc5cd
Devin Quinn <76824496+dquinn6@users.noreply.github.com>
2024-04-01 22:02:36 -0400

    Cleaned helper and communicator modules
    Added docstrings, formatted, and linted app_helpers.py and communicators.py

--- src/app_helpers.py
+++ src/app_helpers.py
@@ -1,129 +1,203 @@
-import os
-import logging
-from logging.handlers import RotatingFileHandler
-from config import config
-import streamlit as st
-from src.utils import update_patterns_json  
+""" Module for UI application helper functions.  """
 
+import logging
+import os
+from logging.handlers import RotatingFileHandler
+from typing import Optional
+
+import streamlit as st
+from config import config
+
+from src import communicators, data_processors
+from src.utils import update_patterns_json
+
+# Define LOG_PATH in program config.yml
 LOG_PATH = config.user_config["LOG_PATH"]
 
-def btn_lock_callback():
-    st.session_state.disable_flg = not st.session_state.disable_flg
 
 # Direct UI logging to streamlit.log through logger object
 def create_logger(
     name: str = "logger_",
     level: str = "INFO",
-    filename: str = LOG_PATH+"streamlit.log",
-    max_log_size: int = 1024 * 1024 * 1024, #100 MB
+    filename: str = LOG_PATH + "streamlit.log",
+    max_log_size: int = 1024 * 1024 * 1024,  # 100 MB
     backup_count: int = 1,
-):
-    """ Create logger for handling streamlit IO
+) -> logging.Logger:
+    """Create a RotatingFileHandler logger for handling streamlit IO. Expects variable LOG_PATH already defined in module.
 
     Solution to duplicate log messages; found at https://discuss.streamlit.io/t/streamlit-duplicates-log-messages-when-stream-handler-is-added/16426
 
     Args:
-        name (str, optional): _description_. Defaults to "logger_".
-        level (str, optional): _description_. Defaults to "DEBUG".
-        filename (str, optional): _description_. Defaults to "app.log".
-        max_log_size (int, optional): _description_. Defaults to 1024*1024*1024.
+        name (str, optional): Name for logging.getLogger(). Defaults to "logger_".
+        level (str, optional): Logging level to display messages. Defaults to "INFO".
+        filename (str, optional): LOG_PATH (module variable) + filename to write log messages to. Defaults to LOG_PATH+"streamlit.log"
+        max_log_size (int, optional): Max size (bytes) for log file before rollover. Defaults to 1024*1024*1024.
+        backup_count (int, optional): number of old log files to maintain. Defaults to 1.
 
     Returns:
-        _type_: _description_
+        logging.Logger: Logger object for UI message logging.
     """
+    # Initialize for new logger object;
     if not os.path.exists(LOG_PATH):
         os.mkdir(LOG_PATH)
     logger = logging.getLogger(name)
     logger.propagate = False
     logger.setLevel(level)
     # If no handler present, add one
-    if(
-        sum([isinstance(handler, RotatingFileHandler) for handler in logger.handlers]) == 0
+    if (
+        sum([isinstance(handler, RotatingFileHandler) for handler in logger.handlers])
+        == 0
     ):
         handler = RotatingFileHandler(
-            filename, maxBytes=max_log_size, backupCount=backup_count,
+            filename,
+            maxBytes=max_log_size,
+            backupCount=backup_count,
         )
         handler.setFormatter(
-            logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+            logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
         )
         logger.addHandler(handler)
-    
+
     return logger
 
+
 def render_doc_viewer(
-        docs_name="documents", 
-        index_name="current_index",
-        include_text=None,
-        unique_key="key",
-    ):
-    
-    docviewer = st.empty() 
-    idx_placeholder = st.empty()
+    docs_name: str = "documents",
+    index_name: str = "current_index",
+    include_text: Optional[str] = None,
+    unique_key: str = "key",
+) -> None:
+    """Helper UI function to render a document viewing window.
 
-    # Initialize the current index
-    if index_name not in st.session_state:
-        st.session_state[index_name] = 0
+    Args:
+        docs_name (str, optional): st.session_state name holding list of docuements. Defaults to "documents".
+        index_name (str, optional): st.session_state name to maintain an object's current index from another. Defaults to "current_index".
+        include_text (Optional[str], optional): Optional text to include in the viewer. Defaults to None.
+        unique_key (str, optional): Unique key name to maintain multiple document viewers at once. Defaults to "key".
+    """
+    try:
+        docviewer = st.empty()
+        idx_placeholder = st.empty()
 
-    # Reset if user filters some docs and current idx is out of bounds
-    if st.session_state[index_name] > len(st.session_state[docs_name]) - 1:
-        st.session_state[index_name] = 0
-
-    show_next = st.button(
-        label="Next document" if not st.session_state.disable_flg else "Disabled during process", 
-        disabled=st.session_state.disable_flg,
-        key=unique_key,
-    )
-
-    # Update index on button click
-    if show_next:
-        # Loop back to beginning if at end of list
-        if st.session_state[index_name] >= len(st.session_state[docs_name]) - 1:
+        # Initialize the current index
+        if index_name not in st.session_state:
             st.session_state[index_name] = 0
+
+        # Reset if user filters some docs and current idx is out of bounds
+        if st.session_state[index_name] > len(st.session_state[docs_name]) - 1:
+            st.session_state[index_name] = 0
+
+        show_next = st.button(
+            label=(
+                "Next document"
+                if not st.session_state.disable_flg
+                else "Disabled during process"
+            ),
+            disabled=st.session_state.disable_flg,
+            key=unique_key,
+        )
+
+        # Update index on button click
+        if show_next:
+            # Loop back to beginning if at end of list
+            if st.session_state[index_name] >= len(st.session_state[docs_name]) - 1:
+                st.session_state[index_name] = 0
+            else:
+                st.session_state[index_name] += 1
+
+        with idx_placeholder.container(border=False):
+            if len(st.session_state[docs_name]) != 0:
+                st.write(
+                    f"Document: {st.session_state[index_name] + 1} / {len(st.session_state[docs_name])}"
+                )
+
+        # Show next element in list
+        with docviewer.container(height=300, border=True):
+            if include_text:
+                st.write(include_text)
+            if len(st.session_state[docs_name]) == 0:
+                st.write(
+                    "No documents with this criteria could be found; try a larger token limit or different search pattern."
+                )
+            else:
+                st.write(st.session_state[docs_name][st.session_state[index_name]])
+
+    except Exception as e:
+        logging.error(f"Failed to render document viewer: {e}")
+
+
+def gather_docs(
+    data_processor: data_processors.DataProcessor,
+    communicator: communicators.Communicator,
+    search_pattern: Optional[str] = None,
+    replace_pattern: Optional[str] = None,
+    verbose: bool = True,
+) -> None:
+    """Helper UI function to find documents with specified search pattern and maintain in session states.
+
+    Args:
+        data_processor (data_processors.DataProcessor): DataProcessor object with data.
+        communicator (communicators.Communicator): Communicator object to count tokens.
+        search_pattern (Optional[str], optional): Search string to find matches for in documents. Defaults to None.
+        replace_pattern (Optional[str], optional): String to replace search string with. Defaults to None.
+        verbose (bool, optional): Write to UI. Defaults to True.
+    """
+    try:
+        if search_pattern is None or replace_pattern is None:
+            if verbose:
+                st.sidebar.write("Please input search and replace patterns")
         else:
-            st.session_state[index_name] += 1
+            update_patterns_json(key=search_pattern, val=replace_pattern)
+            docs_with_pattern = data_processor.ret_passages_with_pattern(search_pattern)
+            if verbose:
+                st.sidebar.write(
+                    f"{len(docs_with_pattern)} / {len(data_processor.data)} documents with this search pattern."
+                )
+            docs_valid = [
+                p
+                for p in docs_with_pattern
+                if communicator.count_tokens(p) < config.user_config["TOKEN_LIMIT"]
+            ]
+            if verbose:
+                st.sidebar.write(
+                    f"{len(docs_valid)} / {len(docs_with_pattern)} documents under token limit."
+                )
+            st.session_state["docs_valid"] = docs_valid
+            st.session_state["docs_with_pattern"] = docs_with_pattern
 
-    with idx_placeholder.container(border=False):
-        if len(st.session_state[docs_name]) != 0:
-            st.write(f"Document: {st.session_state[index_name] + 1} / {len(st.session_state[docs_name])}")
+    except Exception as e:
+        logging.error(f"Failed to gather documents for session states: {e}")
 
-    # Show next element in list
-    with docviewer.container(height=300, border=True):
-        if include_text:
-            st.write(include_text)
-        if len(st.session_state[docs_name]) == 0:
-            st.write("No documents with this criteria could be found; try a larger token limit or different search pattern.")
-        else:
-            st.write(st.session_state[docs_name][st.session_state[index_name]])
 
-def gather_docs(data_processor, communicator, search_pattern=None, replace_pattern=None, verbose=True):
-    if search_pattern == None or replace_pattern == None:
-        if verbose:
-            st.sidebar.write("Please input search and replace patterns")
-    else:
-        update_patterns_json(key=search_pattern, val=replace_pattern)
-        docs_with_pattern = data_processor.ret_passages_with_pattern(search_pattern)
-        if verbose:
-            st.sidebar.write(f"{len(docs_with_pattern)} / {len(data_processor.data)} documents with this search pattern.")
-        docs_valid = [p for p in docs_with_pattern if communicator.count_tokens(p) < config.user_config["TOKEN_LIMIT"]]
-        if verbose:
-            st.sidebar.write(f"{len(docs_valid)} / {len(docs_with_pattern)} documents under token limit.")
-        st.session_state["docs_valid"] = docs_valid
-        st.session_state["docs_with_pattern"] = docs_with_pattern
+def get_model_factory_name(
+    model_name: str = "gpt-3.5-turbo", rag: bool = False
+) -> Optional[str]:
+    """Helper function mapping vendor API model names to program defined names.
 
-def get_model_factory_name(model_name, rag=False):
+    Args:
+        model_name (str, optional): Model name used in vendor API. Defaults to "gpt-3.5-turbo".
+        rag (bool, optional): If true, maps model name to RAG version of model. Defaults to False.
 
+    Returns:
+        Optional[str]: Program defined model if implemented, else None
+    """
     if not rag:
         names_mapped = {
             "gpt-3.5-turbo": "GPT_3.5_TURBO",
             "gpt-4": None,
-            "gpt-4-32k": None
+            "gpt-4-32k": None,
         }
         return names_mapped[model_name]
-    
+
     else:
         names_mapped = {
             "gpt-3.5-turbo": "GPT_3.5_TURBO_RAG",
             "gpt-4": None,
-            "gpt-4-32k": None
+            "gpt-4-32k": None,
         }
         return names_mapped[model_name]
+
+
+def btn_lock_callback():
+    """Callback function to lock UI widgets upon a triggered event."""
+    st.session_state.disable_flg = not st.session_state.disable_flg
--- src/communicators.py
+++ src/communicators.py
@@ -1,58 +1,75 @@
-""" Classes for LLM communication. """
+""" Classes for LLM API communication. """
 
-from openai import OpenAI
-import tiktoken
 import logging
-from typing import Optional
 from abc import ABC, abstractmethod
+from typing import Optional
+
+import tiktoken
+from openai import OpenAI
+
 from src.vectorstore_handlers import VectorstoreHandler
 
+# Define a system RAG role
 RAG_SYS_ROLE_MSG = "You will answer user queries based on the context documents provided. Your responses MUST be grounded from the provided context.\
 YOU WILL LIMIT YOUR KNOWLEDGE ONLY TO THE INFORMATION PROVIDED. YOU WILL NOT PROVIDE ANY EXTERNAL INFORMATION. \
-If information needed to answer the user query is not in the documents provided, you will reply with 'Sorry, I can't answer that based on the provided documents'.\
-"
+If information needed to answer the user query is not in the documents provided, you will reply with 'Sorry, I can't answer that based on the provided documents'."
+
+# Define messages to preface context and query when constructing prompt
 RAG_CONTEXT_PREFACE = "Please use the following context to generate your response, which must not contain outside information:\n\n"
 RAG_QUERY_PREFACE = "\n\nBased solely on the context provided above, please answer the following user query:\n"
 
 
+# Decorator for methods requiring vectorstore to be set
 def vs_required(function):
     def wrapper(self, *args, **kwargs):
-        if self.vs_hndlr == None:
-            raise ValueError("vs_hndlr not set; pass VectostoreHandler upon init or invoke set_vectorstore_handler() before using this method.")
+        if self.vs_hndlr is None:
+            raise ValueError(
+                "vs_hndlr not set; pass VectostoreHandler upon init or invoke set_vectorstore_handler() before using this method."
+            )
         return function(self, *args, **kwargs)
+
     return wrapper
 
 
 class Communicator(ABC):
+    """Base class for Communicator subclasses"""
+
     def __init__(self, vectorstore_handler: Optional[VectorstoreHandler] = None):
+        """Init with optional Vectorstore Handler."""
         self.vs_hndlr = vectorstore_handler
 
-    def set_vectorstore_handler(self, vectorstore_handler: Optional[VectorstoreHandler] = None):
-        """ A method to set the vectorstore handler post-init. """
+    def set_vectorstore_handler(
+        self, vectorstore_handler: Optional[VectorstoreHandler] = None
+    ):
+        """A method to set the vectorstore handler post-init."""
         self.vs_hndlr = vectorstore_handler
 
     @abstractmethod
     def post_prompt(self):
-        """ A method to send/receive messages with LLM, whether through API post or local process. """
+        """A method to send/receive messages with LLM, whether through API post or local process."""
         pass
 
     @abstractmethod
     def count_tokens(self):
-        """ A method to count tokens in text using respective LLM's tokenizer. """
+        """A method to count tokens in text using respective LLM's tokenizer."""
         pass
 
     @abstractmethod
     def post_rag_prompt(self):
-        """ A method for posting with RAG; add vs_required decorator when implementing in subclass """
+        """A method for posting with RAG; add vs_required decorator when implementing in subclass"""
         pass
 
 
 class GPTCommunicator(Communicator):
+    """Communicator subclass for communication with OpenAI GPT models."""
 
     def __init__(
-            self, api_key: str, model_name: str = "gpt-3.5-turbo", vectorstore_handler: Optional[VectorstoreHandler] = None,
-        ) -> None:
-        """ Init object 
+        self,
+        api_key: str,
+        model_name: str = "gpt-3.5-turbo",
+        vectorstore_handler: Optional[VectorstoreHandler] = None,
+    ) -> None:
+        """Init object
 
         Args:
             api_key (str): OpenAI API access token
@@ -61,13 +78,13 @@ class GPTCommunicator(Communicator):
 
         Raises:
             ValueError: Raised when model_name is not a valid GPT model.
-        """        
+        """
         super().__init__(vectorstore_handler)
-        # init client with api key 
+        # init client with api key
         self.client = OpenAI(api_key=api_key)
-        
+
         # context window limits; found at https://platform.openai.com/docs/models
-        model_max_tokens = { 
+        model_max_tokens = {
             "gpt-3.5-turbo": 16385,
             "gpt-4": 8192,
             "gpt-4-32k": 32768,
@@ -75,36 +92,42 @@ class GPTCommunicator(Communicator):
 
         # check for valid model name input
         if model_name not in model_max_tokens.keys():
-            raise ValueError(f"Invalid model name; valid args include: {model_max_tokens.keys()}")
+            raise ValueError(
+                f"Invalid model name; valid args include: {model_max_tokens.keys()}"
+            )
         self.model_name = model_name
 
         # set model attributes
-        self.max_prompt_tokens = model_max_tokens[model_name] -  250 # buffer for response tokens
-        self.system_role = "You are a helpful AI assistant." # default role
+        self.max_prompt_tokens = (
+            model_max_tokens[model_name] - 250
+        )  # buffer for response tokens
+        self.system_role = "You are a helpful AI assistant."  # default role
         self.total_tokens_used = 0
-        self.temperature = 0 # keep as 0 to minimize responses straying from provided documents
-        
-    def post_prompt(self, text: str, truncate: bool=True) -> Optional[str]:
-        """ Method to communicate with GPT.
+        self.temperature = (
+            0  # keep as 0 to minimize responses straying from provided documents
+        )
+
+    def post_prompt(self, text: str, truncate: bool = True) -> Optional[str]:
+        """Method to communicate with GPT.
 
         Args:
             text (str): Input text prompt to send to GPT.
             truncate (bool): If true, will truncate the text to model's token limit before sending.
 
         Returns:
-            Optional[str]: GPT's text response; None if try fails. 
+            Optional[str]: GPT's text response; None if fails.
         """
         try:
             if truncate:
                 text = self.truncate_text(text, token_limit=self.max_prompt_tokens)
 
             response = self.client.chat.completions.create(
-                model = self.model_name,
-                messages = [
+                model=self.model_name,
+                messages=[
                     {"role": "system", "content": str(self.system_role)},
-                    {"role": "user", "content": str(text)}
+                    {"role": "user", "content": str(text)},
                 ],
-                temperature=self.temperature
+                temperature=self.temperature,
             )
             self.last_response = response
             self.total_tokens_used += int(response.usage.total_tokens)
@@ -112,18 +135,18 @@ class GPTCommunicator(Communicator):
         except Exception as e:
             logging.error(f"Failed to post prompt: {e}")
             return None
-        
+
         return response.choices[0].message.content
-    
+
     def count_tokens(self, text: str) -> Optional[int]:
-        """ Method to count number of tokens in a given piece of text. 
+        """Method to count number of tokens in a given piece of text.
 
         Args:
             text (str): Input text to count tokens from.
 
         Returns:
-            Optional[int]: Token count; None if try fails.
-        """        
+            Optional[int]: Token count; None if fails.
+        """
         try:
             encoding = tiktoken.encoding_for_model(self.model_name)
             num_tokens = len(encoding.encode(text))
@@ -132,9 +155,9 @@ class GPTCommunicator(Communicator):
             return None
 
         return num_tokens
-    
+
     def truncate_text(self, text: str, token_limit: int) -> Optional[str]:
-        """ Method to truncate a string to a specified token limit.
+        """Method to truncate a string to a specified token limit.
         Parses by sentence period to not truncate in the middle of a sentence.
 
         Args:
@@ -143,7 +166,7 @@ class GPTCommunicator(Communicator):
 
         Returns:
             Optional[str]: Truncated text; None if try fails.
-        """        
+        """
         token_count = 0
         truncated_text = ""
         try:
@@ -151,7 +174,7 @@ class GPTCommunicator(Communicator):
                 if line.strip() in [""]:
                     continue
 
-                line = line + "." # add . back after split
+                line = line + "."  # add . back after split
 
                 token_count += self.count_tokens(line)
                 if token_count >= token_limit:
@@ -164,20 +187,31 @@ class GPTCommunicator(Communicator):
         except Exception as e:
             logging.error(f"Failed to truncate text: {e}")
             return None
-        
+
         return truncated_text
-    
+
     @vs_required
     def post_rag_prompt(self, query: str):
-        
+        """Method to communicate with GPT using RAG-constructed prompts.
+
+        Args:
+            query (str): Input question ask GPT.
+
+        Returns:
+            Optional[str]: GPT's text response; None if try fails.
+        """
+
         if self.system_role != RAG_SYS_ROLE_MSG:
             self.system_role = RAG_SYS_ROLE_MSG
 
+        # Retrieve top matched documents from vectorstore
         top_context = self.vs_hndlr.retrieve_top_documents(query)
+
+        # Combine list of context docuements into a single string and add preface messages
         all_context = RAG_CONTEXT_PREFACE + "\n\n".join(top_context)
         query = RAG_QUERY_PREFACE + query
 
-        # cut context down if needed
+        # Cut context down if needed; account for size of query
         buffer_token_space = self.count_tokens(query)
         token_limit = self.max_prompt_tokens - buffer_token_space
 
@@ -186,6 +220,8 @@ class GPTCommunicator(Communicator):
         else:
             prompt = all_context + query
 
-        response = self.post_prompt(prompt)
+        response = self.post_prompt(
+            prompt
+        )  # can't use truncate arg here, since it would cut our query at the bottom
 
         return response, top_context
