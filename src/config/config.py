d5ec58e629274ebe324ea7d666119ebaf0113e23
Devin Quinn <76824496+dquinn6@users.noreply.github.com>
2024-04-08 21:07:41 -0400

    Update config.py
    Added default config.yml creation if the file doesn't exist.

--- src/config/config.py
+++ src/config/config.py
@@ -1,6 +1,21 @@
 """Module to load values from config.yml and init in module variable for codebase reference."""
 
 import yaml
+import os
+
+DEFAULT_CONFIG = {
+    "ACCESS_TOKEN": None,
+    "MODEL_NAME": "gpt-3.5-turbo",
+    "DATASET_NAME": "WikiText",
+    "VECTORSTORE_NAME": "LangchainFAISS",
+    "LOG_PATH": "logs/",
+    "PATTERNS_FILENAME": "src/config/manipulate_patterns.json",
+    "SAVE_PATH": "document_store/",
+    "SEARCH_TYPE": "similarity",
+    "N_RETRIEVED_DOCS": 5,
+    "TOKEN_LIMIT": 2000,
+    "VERBOSE": True,
+}
 
 
 class ConfigLoadError(Exception):
@@ -32,4 +47,9 @@ def load_config_yml(filepath: str) -> dict:
 
 # Store user config as var
 filepath = "src/config/user_config.yml"
+
+if not os.path.exists(filepath):
+    with open(filepath, "w") as f:
+        f.write(yaml.dump(DEFAULT_CONFIG))
+
 user_config = load_config_yml(filepath)
